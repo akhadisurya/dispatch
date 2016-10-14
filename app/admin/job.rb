e5bbbd75367560a87e1db7ff6902f4c64d988fc4
Arvin <akhadisurya@gmail.com>
2016-10-14 14:55:21 -0700

    modify active admin job

--- app/admin/job.rb
+++ app/admin/job.rb
@@ -12,6 +12,6 @@ ActiveAdmin.register Job do
 #   permitted << :other if params[:action] == 'create' && current_user.admin?
 #   permitted
 # end
-
+permit_params :name, :technician, :customer, :location, :date, :time
 
 end
